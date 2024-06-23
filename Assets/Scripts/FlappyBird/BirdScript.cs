9be14be388aa453a1db962237a6af1c38d40c1d2
aniketboghum <boghumaniket@gmail.com>
2024-06-23 11:25:57 +0530

    made changes for better naming convention

--- Assets/Scripts/FlappyBird/BirdScript.cs
+++ Assets/Scripts/FlappyBird/BirdScript.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
-    private Rigidbody2D _BirdRigidBody;
-    [SerializeField] private float BirdVelocityStrength;
-    private bool isBirdAlive = true;
+    private Rigidbody2D _birdRigidBody;
+    [SerializeField] private float _birdVelocityStrength;
+    public bool IsBirdAlive { private set; get;} = true;
 
     void Start()
     {
-        _BirdRigidBody = GetComponent<Rigidbody2D>();
+        _birdRigidBody = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && isBirdAlive)    _BirdRigidBody.velocity = Vector2.up * BirdVelocityStrength;   
+        if (Input.GetMouseButtonDown(0) && IsBirdAlive)    _birdRigidBody.velocity = Vector2.up * _birdVelocityStrength;   
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {   
         // Destroy(GetComponent<Rigidbody2D>());
         GameManager.Instance.ShowGameOverPanel();
-        isBirdAlive = false;
+        IsBirdAlive = false;
         GameManager.Instance.ReserPlayerScore();
     }
 }
--- Assets/Scripts/FlappyBird/ScoreIncreamentScript.cs
+++ Assets/Scripts/FlappyBird/ScoreIncreamentScript.cs
@@ -3,6 +3,6 @@ public class ScoreIncreamentScript : MonoBehaviour
 {    
     void OnTriggerEnter2D(Collider2D bird)
     {
-        if (bird.gameObject.layer == 3)    GameManager.Instance.IncrementPlayerScore(1);
+        if (bird.gameObject.layer == 3 && bird.GetComponent<BirdScript>().IsBirdAlive)    GameManager.Instance.IncrementPlayerScore(1);
     }
 }
